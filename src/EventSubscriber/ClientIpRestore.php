19ac3435500c6ee6880db6124dc63738678fe446
Adam Weingarten <adam.weingarten@2677413.no-reply.drupal.org>
2015-12-20 09:24:30 -0500

    Added functionality to restore the originating client's IP address.

---
+++ src/EventSubscriber/ClientIpRestore.php
@@ -0,0 +1,188 @@
+<?php
+
+/**
+ * @file
+ * Contains Drupal\cloudflare\EventSubscriber\ClientIpRestore.
+ */
+
+namespace Drupal\cloudflare\EventSubscriber;
+
+use Drupal\Core\Url;
+use Drupal\Core\Cache\Cache;
+use Drupal\Core\Cache\CacheBackendInterface;
+use Drupal\Core\Config\ConfigFactoryInterface;
+use Drupal\Core\StringTranslation\StringTranslationTrait;
+use GuzzleHttp\ClientInterface;
+use GuzzleHttp\Exception\RequestException;
+use Symfony\Component\HttpKernel\KernelEvents;
+use Symfony\Component\HttpKernel\Event\GetResponseEvent;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpFoundation\IpUtils;
+use Psr\Log\LoggerInterface;
+
+/**
+ * Restores the true client Ip address.
+ *
+ * @see https://support.cloudflare.com/hc/en-us/articles/200170986-How-does-CloudFlare-handle-HTTP-Request-headers-
+ */
+class ClientIpRestore implements EventSubscriberInterface {
+  use StringTranslationTrait;
+
+  const CLOUDFLARE_RANGE_KEY = 'cloudflare_range_key';
+  const CLOUDFLARE_CLIENT_IP_RESTORE_ENABLED = 'client_ip_restore_enabled';
+  const CLOUDFLARE_BYPASS_HOST = 'bypass_host';
+  const IPV4_ENDPOINTS_URL = 'https://www.cloudflare.com/ips-v4';
+  const IPV6_ENDPOINTS_URL = 'https://www.cloudflare.com/ips-v6';
+
+  /**
+   * Cache backend service.
+   *
+   * @var \Drupal\Core\Cache\CacheBackendInterface;
+   */
+  protected $cache;
+
+  /**
+   * The settings configuration.
+   *
+   * @var \Drupal\Core\Config\Config
+   */
+  protected $config;
+
+  /**
+   * The HTTP client to fetch the feed data with.
+   *
+   * @var \GuzzleHttp\ClientInterface
+   */
+  protected $httpClient;
+
+  /**
+   * A logger instance.
+   *
+   * @var \Psr\Log\LoggerInterface
+   */
+  protected $logger;
+
+  /**
+   * TRUE/FALSE if client ip restoration enabled.
+   *
+   * @var bool
+   */
+  protected $isClientIpRestoreEnabled;
+
+  /**
+   * Constructs a UpdateFetcher.
+   *
+   * @param \Drupal\Core\Config\ConfigFactoryInterface $config
+   *   The factory for configuration objects.
+   * @param \Drupal\Core\Cache\CacheBackendInterface $cache
+   *   Cache backend.
+   * @param \GuzzleHttp\ClientInterface $http_client
+   *   A Guzzle client object.
+   * @param \Psr\Log\LoggerInterface $logger
+   *   A logger instance.
+   */
+  public function __construct(ConfigFactoryInterface $config, CacheBackendInterface $cache, ClientInterface $http_client, LoggerInterface $logger) {
+    $this->httpClient = $http_client;
+    $this->cache = $cache;
+    $this->config = $config->get('cloudflare.settings');
+    $this->logger = $logger;
+    $this->isClientIpRestoreEnabled = $this->config->get(SELF::CLOUDFLARE_CLIENT_IP_RESTORE_ENABLED);
+    $this->bypassHost = $this->config->get(SELF::CLOUDFLARE_BYPASS_HOST);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function getSubscribedEvents() {
+    $events[KernelEvents::REQUEST][] = array('onRequest', 20);
+    return $events;
+  }
+
+  /**
+   * Restores the origination client IP delivered to Drupal from CloudFlare.
+   */
+  public function onRequest(GetResponseEvent $event) {
+    if (!$this->isClientIpRestoreEnabled) {
+      return;
+    }
+    $current_request = $event->getRequest();
+    $cf_connecting_ip = $current_request->server->get('HTTP_CF_CONNECTING_IP');
+    $has_http_cf_connecting_ip = !empty($cf_connecting_ip);
+    $has_bypass_host = !empty($this->bypassHost);
+    $client_ip = $current_request->getClientIp();
+    $incoming_uri = $current_request->getHost();
+    $request_expected_to_bypass_cloudflare = $has_bypass_host && $this->bypassHost == $incoming_uri;
+
+    if ($request_expected_to_bypass_cloudflare) {
+      return;
+    }
+
+    if (!$has_http_cf_connecting_ip) {
+      $message = $this->t("Request came through without being routed through CloudFlare.");
+      $this->logger->warning($message);
+      return;
+    }
+
+    $has_ip_already_changed = $client_ip == $cf_connecting_ip;
+
+    // Some environments may make the alteration for us. In which case no
+    // action is required.
+    if ($has_ip_already_changed) {
+      $url_to_settings = Url::fromRoute('cloudflare.admin_settings_form');
+      $link_to_settings = $url_to_settings->getInternalPath();
+      $message = $this->t('Request has already been updated.  This functionality should be deactivated. Please go <a href="@link_to_settings">here</a> to disable "Restore Client Ip Address".', ['@link_to_settings' => $link_to_settings]);
+      $this->logger->warning($message);
+      return;
+    }
+
+    $cloudflare_ipranges = $this->getCloudFlareIpRanges();
+    $request_originating_from_cloudflare = IpUtils::checkIp($client_ip, $cloudflare_ipranges);
+
+    if ($has_http_cf_connecting_ip && !$request_originating_from_cloudflare) {
+      $message = $this->t("Client IP of $client_ip does not match a known CloudFlare IP but there is HTTP_CF_CONNECTING_IP of $cf_connecting_ip.");
+      $this->logger->warning($message);
+      return;
+    }
+
+    $event->getRequest()->server->set('REMOTE_ADDR', $cf_connecting_ip);
+    $event->getRequest()->overrideGlobals();
+  }
+
+  /**
+   * Get a list of cloudflare IP Ranges.
+   *
+   * @return array
+   *   Listing of the CloudFlareIP edge server IP ranges
+   */
+  public function getCloudFlareIpRanges() {
+    if ($cache = $this->cache->get(self::CLOUDFLARE_RANGE_KEY)) {
+      return $cache->data;
+    }
+
+    try {
+      $ipv4_raw_listings = trim((string) $this->httpClient
+        ->get(SELF::IPV4_ENDPOINTS_URL)
+        ->getBody());
+
+      $ipv6_raw_listings = trim((string) $this->httpClient
+        ->get(SELF::IPV6_ENDPOINTS_URL)
+        ->getBody());
+
+      $iv4_endpoints = explode("\n", $ipv4_raw_listings);
+      $iv6_endpoints = explode("\n", $ipv6_raw_listings);
+      $cloudflare_ips = array_merge($iv4_endpoints, $iv6_endpoints);
+      $cloudflare_ips = array_map('trim', $cloudflare_ips);
+
+      if (empty($cloudflare_ips)) {
+        $this->logger->error("Unable to get a listing of CloudFlare IPs.");
+        return [];
+      }
+      $this->cache->set(SELF::CLOUDFLARE_RANGE_KEY, $cloudflare_ips, Cache::PERMANENT);
+      return $cloudflare_ips;
+    }
+    catch (RequestException $exception) {
+      $this->logger->error("Unable to get a listing of CloudFlare IPs. " . $exception->getMessage());
+    }
+  }
+
+}
--- src/Form/CloudFlareAdminSettingsForm.php
+++ src/Form/CloudFlareAdminSettingsForm.php
@@ -19,6 +19,7 @@ use Psr\Log\LoggerInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use CloudFlarePhpSdk\ApiEndpoints\ZoneApi;
 use CloudFlarePhpSdk\Exceptions\CloudFlareException;
+use \InvalidArgumentException;
 
 /**
  * Class CloudFlareAdminSettingsForm.
@@ -122,6 +123,11 @@ class CloudFlareAdminSettingsForm extends ConfigFormBase implements ContainerInj
       '#title' => $this->t('API Credentials'),
     ];
 
+    $form['cloudflare_config'] = [
+      '#type' => 'fieldset',
+      '#title' => $this->t('Configuration'),
+    ];
+
     $form['api_credentials_fieldset']['apikey'] = [
       '#type' => 'textfield',
       '#title' => $this->t('CloudFlare API Key'),
@@ -135,6 +141,19 @@ class CloudFlareAdminSettingsForm extends ConfigFormBase implements ContainerInj
       '#default_value' => $this->config->get('email'),
       '#required' => TRUE,
     ];
+    $form['cloudflare_config']['client_ip_restore_enabled'] = [
+      '#type' => 'checkbox',
+      '#title' => $this->t('Restore Client Ip Address'),
+      '#description' => $this->t('CloudFlare operates as a reverse proxy and replaces the client IP address. This setting will restore it.<br /> Read more <a href="https://support.cloudflare.com/hc/en-us/articles/200170986-How-does-CloudFlare-handle-HTTP-Request-headers-">here</a>.'),
+      '#default_value' => $this->config->get('client_ip_restore_enabled'),
+    ];
+
+    $form['cloudflare_config']['bypass_host'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Host to Bypass CloudFlare'),
+      '#description' => $this->t('Optional: You can specify a host(no http/https) used for authenticated users to edit the site that bypasses CloudFlare. <br /> This will help suppress watchdog warnings regarding requests bypassing CloudFlare.'),
+      '#default_value' => $this->config->get('bypass_host'),
+    ];
 
     return parent::buildForm($form, $form_state);
   }
@@ -145,6 +164,7 @@ class CloudFlareAdminSettingsForm extends ConfigFormBase implements ContainerInj
   public function validateForm(array &$form, FormStateInterface $form_state) {
     $email = $form_state->getValue('email');
     $apikey = $form_state->getValue('apikey');
+    $bypass_host = $form_state->getValue('bypass_host');
     $is_email_valid = $this->emailValidator->isValid($email);
 
     // Using the bare-metal-class here for diagnostic purposes.
@@ -166,6 +186,26 @@ class CloudFlareAdminSettingsForm extends ConfigFormBase implements ContainerInj
       $form_state->setErrorByName('apikey', $this->t('Unfortunately your credentials failed to authenticate against the CloudFlare API.  Please enter valid credentials.'));
     }
 
+
+    try {
+      $has_http = strpos($bypass_host, 'http') > -1;
+      if ($has_http) {
+        $form_state->setErrorByName('$bypass_host', $this->t('Please enter a host without http/https'));
+
+      }
+
+      // Quick and easy way to validate the Domain.
+      if (!empty($bypass_host)) {
+        $bypass_uri = 'http://' . $bypass_host;
+        Url::fromUri($bypass_uri);
+      }
+    }
+
+    catch (InvalidArgumentException $e) {
+      $form_state->setErrorByName('bypass_host', $this->t('You have entered an invalid host.'));
+    }
+
+
     parent::validateForm($form, $form_state);
   }
 
@@ -174,7 +214,6 @@ class CloudFlareAdminSettingsForm extends ConfigFormBase implements ContainerInj
    */
   public function submitForm(array &$form, FormStateInterface $form_state) {
     parent::submitForm($form, $form_state);
-
     $zone_id = $this->getZoneId();
 
     if ($zone_id) {
@@ -182,12 +221,16 @@ class CloudFlareAdminSettingsForm extends ConfigFormBase implements ContainerInj
         ->set('email', $form_state->getValue('email'))
         ->set('zone', $zone_id)
         ->set('valid_credentials', TRUE)
+        ->set('bypass_host', rtrim($form_state->getValue('bypass_host'), "/"))
+        ->set('client_ip_restore_enabled', $form_state->getValue('client_ip_restore_enabled'))
         ->save();
     }
 
     else {
       $this->config->set('apikey', $form_state->getValue('apikey'))
         ->set('email', $form_state->getValue('email'))
+        ->set('bypass_host', rtrim($form_state->getValue('bypass_host'), "/"))
+        ->set('client_ip_restore_enabled', $form_state->getValue('client_ip_restore_enabled'))
         ->save();
     }
 
