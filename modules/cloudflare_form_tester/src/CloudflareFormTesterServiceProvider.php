65aee8d0c8acf8c421cd3d602625bf5f9e4a7d6d
Adam Weingarten <adam.weingarten@2677413.no-reply.drupal.org>
2016-01-24 00:19:55 -0500

    Added cloudflare_form_tester module.

---
+++ modules/cloudflare_form_tester/src/CloudflareFormTesterServiceProvider.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * @file
+ * Contains \Drupal\cloudflare_form_tester\CloudflareFormTesterServiceProvider.
+ */
+
+namespace Drupal\cloudflare_form_tester;
+
+use Drupal\Core\DependencyInjection\ContainerBuilder;
+use Drupal\Core\DependencyInjection\ServiceProviderBase;
+
+/**
+ * Modifies the language manager service.
+ */
+class CloudflareFormTesterServiceProvider extends ServiceProviderBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function alter(ContainerBuilder $container) {
+    // Overrides language_manager class to test domain language negotiation.
+    $definition = $container->getDefinition('cloudflare.composer_dependency_check');
+    $definition->setClass('Drupal\cloudflare_form_tester\Mocks\ComposerDependenciesCheckMock');
+
+    $definition = $container->getDefinition('cloudflare.zone');
+    $definition->setClass('Drupal\cloudflare_form_tester\Mocks\ZoneMock');
+  }
+
+}
---
+++ modules/cloudflare_form_tester/src/Mocks/ComposerDependenciesCheckMock.php
@@ -0,0 +1,84 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\cloudflare\Mocks\ComposerDependenciesCheckMock.
+ */
+
+namespace Drupal\cloudflare_form_tester\Mocks;
+
+use Psr\Log\LoggerInterface;
+use Drupal\cloudflare\Exception\ComposerDependencyException;
+use Drupal\cloudflare\CloudFlareComposerDependenciesCheckInterface;
+
+/**
+ * Tests that composer dependencies are met.
+ */
+class ComposerDependenciesCheckMock implements CloudFlareComposerDependenciesCheckInterface {
+
+  /**
+   * Set the first time that this function is called in a bootstrap.
+   *
+   * @var bool
+   */
+  public static $areDependenciesMet = NULL;
+
+  /**
+   * A logger instance.
+   *
+   * @var \Psr\Log\LoggerInterface
+   */
+  protected $logger;
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(LoggerInterface $logger) {
+    return new static(
+      $logger
+    );
+  }
+
+  /**
+   * ComposerDependenciesCheck constructor.
+   *
+   * @param \Psr\Log\LoggerInterface $logger
+   *   A logger instance.
+   */
+  public function __construct(LoggerInterface $logger) {
+    $this->logger = $logger;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function check() {
+    $are_dependencies_met = \Drupal::state()->get('cloudflaretesting.assertComposerDependenciesMet');
+
+    if (!$are_dependencies_met) {
+      $this->logger->critical(self::ERROR_MESSAGE);
+    }
+
+    return $are_dependencies_met;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function assert() {
+    if (!$this->check()) {
+      throw new ComposerDependencyException(self::ERROR_MESSAGE);
+    }
+  }
+
+  /**
+   * Tells the mock to assert if dependencies are met or not.
+   *
+   * @param bool $are_dependencies_met
+   *   TRUE to mock dependencies are met.  FALSE otherwise.
+   */
+  public static function mockComposerDependenciesMet($are_dependencies_met) {
+    \Drupal::state()->set('cloudflaretesting.assertComposerDependenciesMet', $are_dependencies_met);
+  }
+
+}
