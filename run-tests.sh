029fdfe6309d5c8b749660e584d20fa4ccdb8599
phayes <patrick.d.hayes@gmail.com>
2017-10-03 15:42:43 -0700

    Making sure that profiles and themes directory are created

--- run-tests.sh
+++ run-tests.sh
@@ -3,5 +3,9 @@
 # Copy the program into the drupal installation
 mkdir -p docroot/modules/program && rsync -a . docroot/modules/program  --exclude \".idea\" --exclude bin --exclude \".git\" --exclude \".gitignore\" --exclude docroot --exclude \"*.make\" --exclude \".travis.yml\" --exclude vendor && rm -fr modules/contrib themes/contrib
 
+# Create required directories in docroot
+mkdir -p docroot/profiles
+mkdir -p docroot/themes
+
 # Run the tests
 cd docroot/core && ../../vendor/bin/phpunit --group=cloudflare ../modules/program
