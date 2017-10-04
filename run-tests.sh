1880b0fb6d6b9ad4d0d2ef83a02ef931469f6060
phayes <patrick.d.hayes@gmail.com>
2017-10-04 13:54:00 -0700

    Running tests within docroot, not core

--- run-tests.sh
+++ run-tests.sh
@@ -8,7 +8,7 @@ mkdir -p docroot/profiles
 mkdir -p docroot/themes
 
 # move into docroot
-cd docroot/core
+cd docroot
 
 # Run everything in a subshell so we can always cleanup
 (
@@ -16,24 +16,24 @@ cd docroot/core
   set -e
 
   # Run unit tests, this will complete very quickly and will catch early failures
-  ../../vendor/bin/phpunit --group=cloudflare ../modules/program
+  ../vendor/bin/phpunit --group=cloudflare --configuration=core/phpunit.xml.dist modules/program
 
   # Unit tests passed, boot up a full drupal and run tests
-  ../../vendor/bin/drush site-install standard --yes --account-pass=admin --db-url=mysql://root:@127.0.0.1/simpletest_db
-  ../../vendor/bin/drush config-set system.performance css.preprocess 0 --yes
-  ../../vendor/bin/drush config-set system.performance js.preprocess 0 --yes
-  ../../vendor/bin/drush config-set system.logging error_level all --yes
-  ../../vendor/bin/drush en simpletest cloudflare cloudflarepurger --yes
+  ../vendor/bin/drush site-install standard --yes --account-pass=admin --db-url=mysql://root:@127.0.0.1/simpletest_db
+  ../vendor/bin/drush config-set system.performance css.preprocess 0 --yes
+  ../vendor/bin/drush config-set system.performance js.preprocess 0 --yes
+  ../vendor/bin/drush config-set system.logging error_level all --yes
+  ../vendor/bin/drush en simpletest cloudflare cloudflarepurger --yes
 
   # Boot up server and client
-  ../../vendor/bin/drush runserver --default-server=builtin 8888 > /dev/null &
+  ../vendor/bin/drush runserver --default-server=builtin 8888 > /dev/null &
   echo $! > drush_runserver.pid
   phantomjs --webdriver=4444 > /dev/null &
   echo $! > pantomjs.pid
 
   # Run all tests
-  php scripts/run-tests.sh --module cloudflare --php $(which php) --url http://localhost:8888/ --verbose
-  php scripts/run-tests.sh --module cloudflarepurger --php $(which php) --url http://localhost:8888/ --verbose
+  php core/scripts/run-tests.sh --module cloudflare --php $(which php) --url http://localhost:8888/ --verbose
+  php core/scripts/run-tests.sh --module cloudflarepurger --php $(which php) --url http://localhost:8888/ --verbose
 )
 
 # Store the exit status of the subcommand
