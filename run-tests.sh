97fc09c23d9034a0482de3f6d9c574c904a465b2
phayes <patrick.d.hayes@gmail.com>
2017-10-05 10:35:18 -0700

    Updating as per comments from @varvashenya

--- run-tests.sh
+++ run-tests.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Copy the program into the drupal installation
-mkdir -p docroot/modules/program && rsync -a . docroot/modules/program  --exclude \".idea\" --exclude bin --exclude \".git\" --exclude \".gitignore\" --exclude docroot --exclude \"*.make\" --exclude \".travis.yml\" --exclude vendor && rm -fr modules/contrib themes/contrib
+mkdir -p docroot/modules/program && rsync -a . docroot/modules/program --exclude \".idea\" --exclude bin --exclude \".git\" --exclude \".gitignore\" --exclude docroot --exclude \"*.make\" --exclude \".travis.yml\" --exclude vendor && rm -fr modules/contrib themes/contrib
 
 # Create required directories in docroot
 mkdir -p docroot/profiles
